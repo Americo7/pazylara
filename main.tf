d5e20d19882a94a2f52cde5e497caa9b2692933d
Rodrigo Americo De La Cruz <165839551+Americo7@users.noreply.github.com>
2025-08-21 11:23:52 -0400

    feat: parameterize image and security rules

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.3.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
@@ -11,247 +12,147 @@ provider "openstack" {
   cloud = "openstack"
 }
 
-# Crear la red privada
-resource "openstack_networking_network_v2" "ctfd_network" {
-  name           = "ctfd-network"
-  admin_state_up = "true"
+locals {
+  allowed_ips = [for u in var.users : u.ip]
+
+  vm_instances = flatten([
+    for group_name, group in var.vm_groups : [
+      for inst_name, inst in group.instances : {
+        key         = "${group_name}-${inst_name}"
+        group       = group_name
+        name        = "${group_name}-vm-${inst_name}"
+        private_ip  = inst.private_ip
+        floating_ip = inst.floating_ip
+      }
+    ]
+  ])
+
+  vm_instances_map = { for inst in local.vm_instances : inst.key => inst }
 }
 
-# Crear la subred
-resource "openstack_networking_subnet_v2" "ctfd_subnet" {
-  name       = "ctfd-subnet"
-  network_id = openstack_networking_network_v2.ctfd_network.id
-  cidr       = "10.0.1.0/24"
-  ip_version = 4
-  dns_nameservers = ["8.8.8.8", "8.8.4.4"]
+resource "openstack_networking_network_v2" "network" {
+  name           = var.network.name
+  admin_state_up = true
 }
 
-# Obtener la red externa
+resource "openstack_networking_subnet_v2" "subnet" {
+  name            = var.network.subnet_name
+  network_id      = openstack_networking_network_v2.network.id
+  cidr            = var.network.cidr
+  ip_version      = 4
+  dns_nameservers = var.network.dns_servers
+}
+
 data "openstack_networking_network_v2" "external" {
-  name = "red_externa_01"
+  name = var.external_network_name
 }
 
-# Crear el router
-resource "openstack_networking_router_v2" "ctfd_router" {
-  name                = "ctfd-router"
+resource "openstack_networking_router_v2" "router" {
+  name                = var.network.router_name
   admin_state_up      = true
   external_network_id = data.openstack_networking_network_v2.external.id
 }
 
-# Conectar la subred al router
-resource "openstack_networking_router_interface_v2" "ctfd_router_interface" {
-  router_id = openstack_networking_router_v2.ctfd_router.id
-  subnet_id = openstack_networking_subnet_v2.ctfd_subnet.id
+resource "openstack_networking_router_interface_v2" "router_interface" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet.id
 }
 
-# Crear flavors personalizados
-resource "openstack_compute_flavor_v2" "ctfd_flavor" {
-  name      = "ctfd-flavor"
-  ram       = "6144"
-  vcpus     = "6"
-  disk      = "50"
-  is_public = "true"
+resource "openstack_compute_flavor_v2" "flavor" {
+  for_each = var.vm_groups
+
+  name      = "${each.key}-flavor"
+  ram       = each.value.flavor.ram
+  vcpus     = each.value.flavor.vcpus
+  disk      = each.value.flavor.disk
+  is_public = true
 }
 
-resource "openstack_compute_flavor_v2" "web_challenges_flavor" {
-  name      = "web-challenges-flavor"
-  ram       = "8192"
-  vcpus     = "10"
-  disk      = "50"
-  is_public = "true"
-}
-
-resource "openstack_compute_flavor_v2" "vpn_flavor" {
-  name      = "vpn-flavor"
-  ram       = "8192"
-  vcpus     = "8"
-  disk      = "30"
-  is_public = "true"
-}
-
-# Obtener la imagen de Debian 12
-data "openstack_images_image_v2" "debian12" {
-  name        = "Debian-12-Generic"
+data "openstack_images_image_v2" "image" {
+  name        = var.image_name
   most_recent = true
 }
 
-# Crear security group
-resource "openstack_networking_secgroup_v2" "ctfd_secgroup" {
+resource "openstack_networking_secgroup_v2" "secgroup" {
   name        = "ctfd-secgroup"
   description = "Security group for CTFD infrastructure"
 }
 
-# 1) INGRESS desde el mismo SG (lo que en Horizon se ve como remote: default)
 resource "openstack_networking_secgroup_rule_v2" "ingress_self_ipv4" {
   direction         = "ingress"
   ethertype         = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
-  remote_group_id   = openstack_networking_secgroup_v2.ctfd_secgroup.id
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+  remote_group_id   = openstack_networking_secgroup_v2.secgroup.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "ingress_self_ipv6" {
   direction         = "ingress"
   ethertype         = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
-  remote_group_id   = openstack_networking_secgroup_v2.ctfd_secgroup.id
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+  remote_group_id   = openstack_networking_secgroup_v2.secgroup.id
 }
 
-
-
-# 3) TCP SOLO desde IPs permitidas (IPv4)
 resource "openstack_networking_secgroup_rule_v2" "tcp_ipv4_any_from_allowed" {
-  count             = length(var.allowed_ips)
+  count             = length(local.allowed_ips)
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 1
   port_range_max    = 65535
-  remote_ip_prefix  = "${var.allowed_ips[count.index]}/32"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
+  remote_ip_prefix  = format("%s/32", local.allowed_ips[count.index])
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
 }
 
-# 3.5) UDP SOLO desde IPs permitidas (IPv4)
 resource "openstack_networking_secgroup_rule_v2" "udp_ipv4_any_from_allowed" {
-  count             = length(var.allowed_ips)
+  count             = length(local.allowed_ips)
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "udp"
   port_range_min    = 1
   port_range_max    = 65535
-  remote_ip_prefix  = "${var.allowed_ips[count.index]}/32"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
+  remote_ip_prefix  = format("%s/32", local.allowed_ips[count.index])
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
 }
 
-# 3) ICMP (ping) SOLO desde IPs permitidas (IPv4)
-resource "openstack_networking_secgroup_rule_v2" "icmp_ipv4_restricted" {
-  count             = length(var.allowed_ips)
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "icmp"
-  # Deja sin port_range_min/max para permitir todos los tipos/códigos ICMP
-  remote_ip_prefix  = "${var.allowed_ips[count.index]}/32"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
+resource "openstack_networking_secgroup_rule_v2" "custom" {
+  for_each = var.security_group_rules
+
+  direction         = each.value.direction
+  ethertype         = each.value.ethertype
+  protocol          = each.value.protocol
+  port_range_min    = each.value.port_range_min
+  port_range_max    = each.value.port_range_max
+  remote_ip_prefix  = each.value.remote_ip_prefix
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
 }
 
-# 4) HTTP/HTTPS abiertos al mundo (si así lo necesitas para CTFD)
-resource "openstack_networking_secgroup_rule_v2" "http" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 80
-  port_range_max    = 80
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "https" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
-}
-
-# (Opcional) WireGuard: puedes mantenerlo abierto o restringirlo también a allowed_ips
-resource "openstack_networking_secgroup_rule_v2" "wireguard_any" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "udp"
-  port_range_min    = 51820
-  port_range_max    = 51820
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.ctfd_secgroup.id
-}
-
-# VMs para CTFD (2 máquinas) con IPs específicas
-resource "openstack_compute_instance_v2" "ctfd_vm" {
-  count           = 2
-  name            = "ctfd-vm-${count.index + 1}"
-  image_id        = data.openstack_images_image_v2.debian12.id
-  flavor_id       = openstack_compute_flavor_v2.ctfd_flavor.id
-  key_pair        = null  # Las claves SSH se configuran via cloud-init
-  security_groups = [openstack_networking_secgroup_v2.ctfd_secgroup.name]
+resource "openstack_compute_instance_v2" "vm" {
+  for_each        = local.vm_instances_map
+  name            = each.value.name
+  image_id        = data.openstack_images_image_v2.image.id
+  flavor_id       = openstack_compute_flavor_v2.flavor[each.value.group].id
+  key_pair        = null
+  security_groups = [openstack_networking_secgroup_v2.secgroup.name]
   user_data       = file("cloud-init.yaml")
 
   network {
-    name        = openstack_networking_network_v2.ctfd_network.name
-    fixed_ip_v4 = var.private_ips.ctfd[count.index]
+    name        = openstack_networking_network_v2.network.name
+    fixed_ip_v4 = each.value.private_ip
   }
 
-  depends_on = [openstack_networking_router_interface_v2.ctfd_router_interface]
+  depends_on = [openstack_networking_router_interface_v2.router_interface]
 }
 
-# VMs para Web Challenges (2 máquinas) con IPs específicas
-resource "openstack_compute_instance_v2" "web_challenges_vm" {
-  count           = 2
-  name            = "web-challenges-vm-${count.index + 1}"
-  image_id        = data.openstack_images_image_v2.debian12.id
-  flavor_id       = openstack_compute_flavor_v2.web_challenges_flavor.id
-  key_pair        = null  # Las claves SSH se configuran via cloud-init
-  security_groups = [openstack_networking_secgroup_v2.ctfd_secgroup.name]
-  user_data       = file("cloud-init.yaml")
+resource "openstack_networking_floatingip_v2" "fip" {
+  for_each = { for k, v in local.vm_instances_map : k => v if v.floating_ip != "" }
 
-  network {
-    name        = openstack_networking_network_v2.ctfd_network.name
-    fixed_ip_v4 = var.private_ips.web_challenges[count.index]
-  }
-
-  depends_on = [openstack_networking_router_interface_v2.ctfd_router_interface]
+  pool    = var.external_network_name
+  address = each.value.floating_ip
 }
 
-# VM para VPN WireGuard con IP específica
-resource "openstack_compute_instance_v2" "vpn_vm" {
-  name            = "vpn-wireguard-vm"
-  image_id        = data.openstack_images_image_v2.debian12.id
-  flavor_id       = openstack_compute_flavor_v2.vpn_flavor.id
-  key_pair        = null  # Las claves SSH se configuran via cloud-init
-  security_groups = [openstack_networking_secgroup_v2.ctfd_secgroup.name]
-  user_data       = file("cloud-init.yaml")
+resource "openstack_compute_floatingip_associate_v2" "fip_assoc" {
+  for_each = openstack_networking_floatingip_v2.fip
 
-  network {
-    name        = openstack_networking_network_v2.ctfd_network.name
-    fixed_ip_v4 = var.private_ips.vpn
-  }
-
-  depends_on = [openstack_networking_router_interface_v2.ctfd_router_interface]
-}
-
-# Floating IPs condicionales - solo se crean si se especifica una IP
-resource "openstack_networking_floatingip_v2" "ctfd_floating_ip" {
-  count   = length([for ip in var.floating_ips.ctfd : ip if ip != ""])
-  pool    = "red_externa_01"
-  address = [for ip in var.floating_ips.ctfd : ip if ip != ""][count.index]
-}
-
-resource "openstack_networking_floatingip_v2" "web_challenges_floating_ip" {
-  count   = length([for ip in var.floating_ips.web_challenges : ip if ip != ""])
-  pool    = "red_externa_01"
-  address = [for ip in var.floating_ips.web_challenges : ip if ip != ""][count.index]
-}
-
-resource "openstack_networking_floatingip_v2" "vpn_floating_ip" {
-  count   = var.floating_ips.vpn != "" ? 1 : 0
-  pool    = "red_externa_01"
-  address = var.floating_ips.vpn
-}
-
-# Asociar floating IPs a las VMs - solo si existen
-resource "openstack_compute_floatingip_associate_v2" "ctfd_fip_associate" {
-  count       = length(openstack_networking_floatingip_v2.ctfd_floating_ip)
-  floating_ip = openstack_networking_floatingip_v2.ctfd_floating_ip[count.index].address
-  instance_id = openstack_compute_instance_v2.ctfd_vm[count.index].id
-}
-
-resource "openstack_compute_floatingip_associate_v2" "web_challenges_fip_associate" {
-  count       = length(openstack_networking_floatingip_v2.web_challenges_floating_ip)
-  floating_ip = openstack_networking_floatingip_v2.web_challenges_floating_ip[count.index].address
-  instance_id = openstack_compute_instance_v2.web_challenges_vm[count.index].id
-}
-
-resource "openstack_compute_floatingip_associate_v2" "vpn_fip_associate" {
-  count       = length(openstack_networking_floatingip_v2.vpn_floating_ip)
-  floating_ip = openstack_networking_floatingip_v2.vpn_floating_ip[0].address
-  instance_id = openstack_compute_instance_v2.vpn_vm.id
+  floating_ip = each.value.address
+  instance_id = openstack_compute_instance_v2.vm[each.key].id
 }
